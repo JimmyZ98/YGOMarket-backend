56eab86b1cf17d3b6f71324e51b09f626c5ebdd4
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2022-07-05 11:09:22 -0400

    saving progress for deployment

--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -3,51 +3,79 @@ const fs = require("fs");
 const router = express.Router();
 const knex = require("knex")(require("../knexfile").development);
 
-router
-  .route("/")
-  .get((req, res) => {
-    knex("listing")
-      .then((data) => {
+router.route("/").get((req, res) => {
+  knex("listing")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error retrieving sales posts: ${err}`)
+    );
+});
+
+router.route("/sell").post((req, res) => {
+  const {
+    id,
+    cardName,
+    cardCode,
+    rarity,
+    description,
+    image,
+    marketPrice,
+    price,
+    sellerId,
+  } = req.body;
+  const newListing = {
+    id,
+    cardName,
+    cardCode,
+    rarity,
+    description,
+    image,
+    marketPrice,
+    price,
+    sellerId,
+  };
+  knex
+    .insert(newListing)
+    .into("listing")
+    .then(() => {
+      knex("listing").then((data) => {
         res.status(200).json(data);
-      })
-      .catch((err) =>
-        res.status(400).send(`Error retrieving sales posts: ${err}`)
-      );
-  })
-  .post((req, res) => {
-    const {
-      id,
-      cardName,
-      cardCode,
-      rarity,
-      description,
-      image,
-      marketPrice,
-      price,
-      sellerId,
-    } = req.body;
-    const newListing = {
-      id,
-      cardName,
-      cardCode,
-      rarity,
-      description,
-      image,
-      marketPrice,
-      price,
-      sellerId,
-    };
-    knex
-      .insert(newListing)
-      .into("listing")
-      .then(() => {
-        knex("listing").then((data) => {
-          res.status(200).json(data);
-        });
-      })
-      .catch((err) => {
-        res.status(400).send(`Error adding sales post: ${err}`);
       });
-  });
+    })
+    .catch((err) => {
+      res.status(400).send(`Error adding sales post: ${err}`);
+    });
+});
+
+// router.route("/checkout").post((req, res) => {
+//   knex("listing")
+//     .whereIn("listing.id", req.body)
+//     .del()
+//     .then((data) => {
+//       res.status(200).json(data);
+//     })
+
+//     .catch((err) => {
+//       res.status(400).send(`Error checking out: ${err}`);
+//     });
+// });
+
+router.route("/checkout").post((req, res) => {
+  res.send(req.body);
+});
+
+// router.route("/checkout").delete((req, res) => {
+//   knex("listing")
+//     .whereIn("listing.id", [req.body.id])
+//     .del()
+//     .then((data) => {
+//       res.status(200).json(data);
+//     })
+//     .catch((err) => {
+//       res.status(400).send(`Error checking out: ${err}`);
+//     });
+// });
 
 module.exports = router;
